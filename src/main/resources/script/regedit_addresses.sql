3505554c24013b0e76c3b9ab92608e799ea11e76
renzo80 <iannacone@istat.it>
2020-11-24 10:43:14 +0100

    Added test data for addresses table

---
+++ src/main/resources/script/regedit_addresses.sql
@@ -0,0 +1,28 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: regedit
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+
+
+--
+-- Dumping data for table `addresses`
+--
+
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+INSERT INTO `addresses` VALUES (1,'40','100','Roma','','Via di Monteverde 135','','via','di Monteverde','135','','SI','','','','','','','',''),(2,'40','101','Roma','','Via di Monteverde 140/A','','via di','Monteverde','140A','','NO','VIA','di Monteverde','140','A','','','',''),(3,'40','6869314','Fondi','','VIA SPINETE I (3\' TRATTO) 9/A','','VIA','SPINETE I','3','TRATTO 9 A ','NO','VIA','SPINETE I','9','A ','','1','10',''),(4,'40','6859048','Monte San Biagio','','VIA CARRO 1A TRAVERSA 10 A','','VIA','CARRO','1','A TRAVERSA 10 A ','NO','VIA','CARRO','10',' A','','3','5',''),(5,'40','2406943','Merano/Meran','','VIA XXIV MAGGIO-24.MAISTRASSE','','VIA','IV NOVEMBRE','4','N OVEMBERSTRASSE 42 ','NO','VIA','IV NOVEMBRE','42','','','','',''),(6,'40','36735799','Catania','','VIA BARTOLOMEO ALTAVILLA ISOL.A 14','','VIA','BARTOLOMEO ALTAVILLA','','IS OL A 14 ','NO','VIA','BARTOLOMEO ALTAVILLA','14','','','','',''),(7,'40','5478541','Rignano sull\'Arno','','VIA GEN. CARLO ALBERTO DALLA CHIESA 4','','','','','','NO','VIA','CARLO ALBERTO DALLA CHIESA','9','','','','',''),(8,'40','12923246','Tortona','','STR STR.PROV.POZZOLO F.RO 22 C','','','Strada Vicinale del Pozzo 22','','','','','','','','','','',''),(9,'40','572246','MONTIGLIO MONFERRATO','','VIA ASTI 43','','VIA','ASTI','43','','SI','','','','','','','',''),(10,'40','163','Cavallino','','VIA  ALESSANDRO MANZONI 37 A','','VIA','ALESSANDRO MANZONI','37','A','SI','','','','','','','',''),(11,'40','24414805','Messina','ARIELLA','VIA ARIELLA 10','ARIELLA','VIA','ARIELLA','10','','SI','','','','','','','',''),(12,'40','','','','','','','','','','','','','','','','','',''),(13,'40','','','','','','','','','','','','','','','','','','');
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-24 10:35:34
