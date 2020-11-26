5fe227658db9fdb5f76599961bb3c5fa4d607a82
renzo80 <iannacone@istat.it>
2020-11-26 14:42:40 +0100

    Added EditAddress class and updated AddressDto with lombock import.

---
+++ src/main/java/it/istat/mec/regedit/domain/EditAddress.java
@@ -0,0 +1,41 @@
+package it.istat.mec.regedit.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "EDIT_ADDRESS")
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class EditAddress implements Serializable {
+	
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	@Column(name = "USER")
+	private Integer user;
+	@Column(name = "ADDRESS")
+	private Integer address;
+	@Column(name = "STATE")
+	private String state;
+	@Column(name = "DATE_TIME")
+	private String dateTime;
+}
--- src/main/java/it/istat/mec/regedit/dto/AddressDto.java
+++ src/main/java/it/istat/mec/regedit/dto/AddressDto.java
@@ -22,6 +22,10 @@
  */
 package it.istat.mec.regedit.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
 public class AddressDto {
 	
 	private Long id;	
@@ -44,126 +48,7 @@ public class AddressDto {
 	private String chiaveStrada;	
 	private String chiaveCivico;	
 	private String fonte;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getCodiceArchivioOr() {
-		return codiceArchivioOr;
-	}
-	public void setCodiceArchivioOr(String codiceArchivioOr) {
-		this.codiceArchivioOr = codiceArchivioOr;
-	}
-	public String getProgressivoIndirizzoOr() {
-		return progressivoIndirizzoOr;
-	}
-	public void setProgressivoIndirizzoOr(String progressivoIndirizzoOr) {
-		this.progressivoIndirizzoOr = progressivoIndirizzoOr;
-	}
-	public String getComuneOr() {
-		return comuneOr;
-	}
-	public void setComuneOr(String comuneOr) {
-		this.comuneOr = comuneOr;
-	}
-	public String getLocalitaOr() {
-		return localitaOr;
-	}
-	public void setLocalitaOr(String localitaOr) {
-		this.localitaOr = localitaOr;
-	}
-	public String getIndirizzoOriginale() {
-		return indirizzoOriginale;
-	}
-	public void setIndirizzoOriginale(String indirizzoOriginale) {
-		this.indirizzoOriginale = indirizzoOriginale;
-	}
-	public String getLocalitaSu() {
-		return localitaSu;
-	}
-	public void setLocalitaSu(String localitaSu) {
-		this.localitaSu = localitaSu;
-	}
-	public String getDugSu() {
-		return dugSu;
-	}
-	public void setDugSu(String dugSu) {
-		this.dugSu = dugSu;
-	}
-	public String getDufSu() {
-		return dufSu;
-	}
-	public void setDufSu(String dufSu) {
-		this.dufSu = dufSu;
-	}
-	public String getCivicoSu() {
-		return civicoSu;
-	}
-	public void setCivicoSu(String civicoSu) {
-		this.civicoSu = civicoSu;
-	}
-	public String getEsponenteSu() {
-		return esponenteSu;
-	}
-	public void setEsponenteSu(String esponenteSu) {
-		this.esponenteSu = esponenteSu;
-	}
-	public String getValidazione() {
-		return validazione;
-	}
-	public void setValidazione(String validazione) {
-		this.validazione = validazione;
-	}
-	public String getDug() {
-		return dug;
-	}
-	public void setDug(String dug) {
-		this.dug = dug;
-	}
-	public String getDuf() {
-		return duf;
-	}
-	public void setDuf(String duf) {
-		this.duf = duf;
-	}
-	public String getCivico() {
-		return civico;
-	}
-	public void setCivico(String civico) {
-		this.civico = civico;
-	}
-	public String getEsponente() {
-		return esponente;
-	}
-	public void setEsponente(String esponente) {
-		this.esponente = esponente;
-	}
-	public String getLocalita() {
-		return localita;
-	}
-	public void setLocalita(String localita) {
-		this.localita = localita;
-	}
-	public String getChiaveStrada() {
-		return chiaveStrada;
-	}
-	public void setChiaveStrada(String chiaveStrada) {
-		this.chiaveStrada = chiaveStrada;
-	}
-	public String getChiaveCivico() {
-		return chiaveCivico;
-	}
-	public void setChiaveCivico(String chiaveCivico) {
-		this.chiaveCivico = chiaveCivico;
-	}
-	public String getFonte() {
-		return fonte;
-	}
-	public void setFonte(String fonte) {
-		this.fonte = fonte;
-	}
+	
 	
 	
 }
