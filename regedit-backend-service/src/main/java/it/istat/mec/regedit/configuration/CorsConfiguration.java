07b8bcd8114d6ec43d3f78952c2e43c625d80f85
Francesco Amato <francamato@gmail.com>
2020-12-14 11:10:34 +0100

    fix cors

--- regedit-backend-service/src/main/java/it/istat/mec/regedit/configuration/CorsConfiguration.java
+++ regedit-backend-service/src/main/java/it/istat/mec/regedit/configuration/CorsConfiguration.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*");
+		registry.addMapping("*").allowedMethods("*");
 	}
 }
