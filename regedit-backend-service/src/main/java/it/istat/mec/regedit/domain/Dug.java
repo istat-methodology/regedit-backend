1df3766477bd59edd4d818b6b4326e1433f14669
renzo80 <iannacone@istat.it>
2020-12-16 13:14:18 +0100

    Updated DUG domain

--- regedit-backend-service/src/main/java/it/istat/mec/regedit/domain/Dug.java
+++ regedit-backend-service/src/main/java/it/istat/mec/regedit/domain/Dug.java
@@ -24,7 +24,11 @@ package it.istat.mec.regedit.domain;
  */
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -39,8 +43,12 @@ import lombok.Setter;
 public class Dug implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name = "ID")
 	private Integer id;
+	@Column(name = "NAME")
 	private String name;
+	@Column(name = "DESCRIPTION")
 	private String description;
 }
