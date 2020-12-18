aa61181aa76d27ae6dd097fa90bb8505979b1207
SMacOne <s.macone@libero.it>
2020-12-18 10:25:27 +0100

    fix corse

--- regedit-backend-service/src/main/java/it/istat/mec/regedit/controller/AddressController.java
+++ regedit-backend-service/src/main/java/it/istat/mec/regedit/controller/AddressController.java
@@ -28,6 +28,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -81,7 +82,7 @@ public class AddressController {
 			
 		return addressService.newAdress(request);
 	}
-
+	@CrossOrigin("*")
 	@PutMapping(value = "/addresses/{addressId}")
 	public AddressDto updateAddress(@RequestBody UpdateAddressRequest request) {
 		
