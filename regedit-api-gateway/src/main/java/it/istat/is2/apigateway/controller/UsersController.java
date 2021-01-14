623ce6ebc2969f2fe1c36391a47cd8d95318f738
Francesco Amato <francamato@gmail.com>
2021-01-14 11:21:06 +0100

    fix user

--- regedit-api-gateway/src/main/java/it/istat/is2/apigateway/controller/UsersController.java
+++ regedit-api-gateway/src/main/java/it/istat/is2/apigateway/controller/UsersController.java
@@ -3,12 +3,11 @@ package it.istat.is2.apigateway.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller; 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import it.istat.is2.apigateway.dto.UsersDto;
 import it.istat.is2.apigateway.service.UserService;
@@ -16,17 +15,18 @@ import it.istat.is2.apigateway.service.UserService;
 @Controller
 @RequestMapping("/api")
 public class UsersController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@GetMapping("/users")
-	public ResponseEntity<List<UsersDto>> getAllUsers() {
 
-		return ResponseEntity.ok(userService.findAllUsers());
-
+	@GetMapping("/user/users")
+	@ResponseBody
+	public List<UsersDto> getAllUsers() {
+		return userService.findAllUsers();
 	}
-	@GetMapping(value = "/users/{id}")
+
+	@GetMapping(value = "/user/users/{id}")
+	@ResponseBody
 	public UsersDto getUser(@PathVariable("id") Integer id) {
 
 		return userService.findUserById(id);
