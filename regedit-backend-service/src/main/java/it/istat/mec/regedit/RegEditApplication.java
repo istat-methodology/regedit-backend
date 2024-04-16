b75d38e50f64ef9a1cba4a30ec27e9f6ec757ebe
SMacOne <s.macone@libero.it>
2024-04-16 11:06:27 +0200

    changes

--- regedit-backend-service/src/main/java/it/istat/mec/regedit/RegEditApplication.java
+++ regedit-backend-service/src/main/java/it/istat/mec/regedit/RegEditApplication.java
@@ -11,15 +11,27 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 
 
 @SpringBootApplication
 //@EnableEurekaClient
 
-public class RegEditApplication extends SpringBootServletInitializer {
+public class RegEditApplication extends SpringBootServletInitializer implements CommandLineRunner{
 	//public static RConnection connection = null;
 	//private static final Logger log = LoggerFactory.getLogger(RegEditApplication.class);
+	@Value("${app.editing.Rserver}")
+	private String Rserver;
+	@Value("${app.editing.Rport}")
+	private Integer Rport;
+	@Value("${app.editing.Ruser}")
+	private String Ruser;
+	@Value("${app.editing.Rpass}")
+	private String Rpass;
+	@Value("${app.editing.Rwd}")
+	private String Rwd;
+	static String risultato;
 	public static void main(String[] args) throws RserveException {
 		SpringApplication.run(RegEditApplication.class, args);		
 	}
@@ -33,51 +45,116 @@ public class RegEditApplication extends SpringBootServletInitializer {
 	
 	
 	
-//	@Override
-//	public void run(String... strings) throws REXPMismatchException {
-//		String hostname = "rstudio.istat.it";
-//		int port=6311;
-//		//RConnection connection=null;	
-//		String risultato = null;
-//		
-//
-////	try {
-////		
-////		connection = open(connection, hostname, port, username, password);
-////		System.out.println("Sono nel try");
-////		//connection = new RConnection(hostname,port);
-////		System.out.println("test R: " +connection);
-////	//}catch(RserveException e) {
-////	}catch(Exception e) {
-////		System.out.println("Si è verificata un'eccezione: "+e);
-////	}
-//		try {
-//			RConnection conn=null;
-//			conn = open(null,hostname,port,"ruser","Ruser2023");
-//			System.out.println("Connessione Server R:" + conn.toString());
-//			risultato += conn.eval("3 * 5").asString();
-//			System.out.println("Risultato moltiplicazione R;" + risultato);
-//			//conn.eval("setwd('" +"script/regedit/" + "')");
-//			//risultato += conn.parseAndEval("getwd()").toString();
-//			conn.eval("setwd('//home2//ruser')").toString();
-//			String path = conn.eval("getwd()").asString();
-//			System.out.println("path RServe;" + path);
-//            RList ris = conn.eval("source('script//regedit//prova.R')").asList();
-//			System.out.print(ris);
-//			for(int i=0;i<ris.size();i++){
-//			      System.out.println(ris.at(i).toString());
-//			}
-//			//"source('" + fileScriptR + "')"
-//			System.out.println("Risultato sript R;" + risultato);
-//		} catch (RserveException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	
-//			
-//		
-//	}
-	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Connessione Server R Starting");
+		
+		RConnection conn=null;
+		try {
+			
+			
+			
+				
+				
+				conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
+				System.out.println("Connessione Server R:" + conn.toString());
+				risultato += conn.eval("3 * 5").asString();
+				System.out.println("Risultato moltiplicazione R;" + risultato);
+				conn.close();
+				//conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
+				conn = open(null,Rserver,Rport,Ruser,Rpass);
+				System.out.println("Connessione Server R:" + conn.toString());
+				conn.eval("setwd('//home2//ruser')").toString();
+				//conn.eval("setwd(Rwd)").toString();
+				String path = conn.eval("getwd()").asString();
+				System.out.println("path RServe;" + path);
+				
+				conn.close();
+				conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
+				
+				
+				
+				
+				  //REXPGenericVector ris = (REXPGenericVector)conn.eval("source('script//regedit//prova.R')");
+				  //System.out.println(ris.asList().at("value").asString());
+				 
+					/*
+					 * conn.close();
+					 * 
+					 * System.out.println(ris.asList().at("value").asString()); conn =
+					 * open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
+					 * conn.eval("setwd('//home2//ruser')").toString();
+					 * 
+					 * String path2 = conn.eval("getwd()").asString();
+					 * System.out.println("path RServe;" + path2);
+					 */
+				//REXPGenericVector ris2 = (REXPGenericVector) conn.eval("source('script//regedit//connessionetest.R')");
+				//REXP ris2 = conn.eval("source('script//regedit//connessionetest.R')");
+				  
+				  
+				  
+				  
+				conn.eval("setwd('//home2//ruser')").toString();
+				path = conn.eval("getwd()").asString();
+				System.out.println("path RServe;" + path);
+				//REXPGenericVector ris2 = (REXPGenericVector) conn.eval("source('R_directory_test//LinkageProbabilistico.R')");
+				//System.out.println(ris2.asList().at("value"));
+				REXP rResponseObject =  conn.parseAndEval("try(source('R_directory_test//LinkageProbabilistico.R'), silent=TRUE)");
+				
+				REXP ris2 = conn.eval("outstr");
+				System.out.println(ris2.asString());
+				//System.out.println(rResponseObject.asString());
+				/*
+				 * REXP ris1 = conn.eval("oraodbcname<-'PD_RSI_PT'" );
+				 * System.out.print("Assegnazione DB:" + ris1.asStrings()[0]);
+				 */
+				
+				/*
+				 * REXP ris2 = conn.eval("orauser<-'UP_STEFANO_MACONE'");
+				 * System.out.print("Assegnazione utente DB:" + ris2.asStrings()[0]);
+				 * 
+				 * REXP ris3 = conn.eval("orapassword<-'Malcomx#1971'");
+				 * System.out.print("Assegnazione password DB:" + ris3.asStrings()[0]);
+				 */
+				
+				/*
+				 * REXP ris4 = conn.eval("PROV<-'Sassari'");
+				 * System.out.print("Assegnazione Provincia:" + ris4.asStrings()[0]);
+				 * 
+				 * REXP ris5 = conn.eval("COD<-'090'");
+				 * System.out.print("Assegnazione Codice Provincia:" + ris5.asStrings()[0]);
+				 * 
+				 * REXP ris6 = conn.eval("s<- 0.6"); System.out.print("Assegnazione Soglia" +
+				 * ris6.asStrings()[0]);
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * REXP ris7 = (REXPGenericVector)
+				 * conn.parseAndEval("source('script//regedit//indirizzi_rodbc_regedit_mod.R')")
+				 * ;
+				 * 
+				 * System.out.print("Output Esecuzione Script:" +
+				 * ris7.asList().at("value").asString());
+				 * 
+				 * REXP ris8 = (REXPGenericVector) conn.eval("result')");
+				 * 
+				 * System.out.print("Output Script:" + ris8.asList().at("value").asString());
+				 * conn.close();
+				 */
+				
+				conn.close();
+				
+			
+		} catch (RserveException e) {
+			// TODO Auto-generated catch block
+			conn.close();
+			e.printStackTrace();
+		}
+	}	
 	
 	
 	@Override
@@ -101,6 +178,11 @@ public class RegEditApplication extends SpringBootServletInitializer {
 		};
 	}
 	
+	public static RConnection open(RConnection connection,String hostname,int port,String username,String password) throws RserveException{
+		connection = new RConnection(hostname,port);
+	     connection.login(username,password);
+	      return connection;
+	}
 		
 		// Test connessione con RServe
 		
