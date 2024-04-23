42be065a1fe205c84b6845b44d039ceb7c759dd8
SMacOne <s.macone@libero.it>
2024-04-23 11:04:12 +0200

    modifiche

--- regedit-backend-service/src/main/java/it/istat/mec/regedit/RegEditApplication.java
+++ regedit-backend-service/src/main/java/it/istat/mec/regedit/RegEditApplication.java
@@ -18,7 +18,7 @@ import org.springframework.boot.CommandLineRunner;
 @SpringBootApplication
 //@EnableEurekaClient
 
-public class RegEditApplication extends SpringBootServletInitializer implements CommandLineRunner{
+public class RegEditApplication extends SpringBootServletInitializer{
 	//public static RConnection connection = null;
 	//private static final Logger log = LoggerFactory.getLogger(RegEditApplication.class);
 	@Value("${app.editing.Rserver}")
@@ -36,126 +36,62 @@ public class RegEditApplication extends SpringBootServletInitializer implements 
 		SpringApplication.run(RegEditApplication.class, args);		
 	}
 	
-	String hostname = "rstudio.istat.it";
-	int port=6311;
-	String username="ruser";
-	String password="Ruser2023";
+	/*
+	 * String hostname = "rstudio.istat.it"; int port=6311; String username="ruser";
+	 * String password="Ruser2023";
+	 */
 	
 	
 	
 	
 	
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Connessione Server R Starting");
-		
-		RConnection conn=null;
-		try {
-			
-			
-			
-				
-				
-				conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
-				System.out.println("Connessione Server R:" + conn.toString());
-				risultato += conn.eval("3 * 5").asString();
-				System.out.println("Risultato moltiplicazione R;" + risultato);
-				conn.close();
-				//conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
-				conn = open(null,Rserver,Rport,Ruser,Rpass);
-				System.out.println("Connessione Server R:" + conn.toString());
-				conn.eval("setwd('//home2//ruser')").toString();
-				//conn.eval("setwd(Rwd)").toString();
-				String path = conn.eval("getwd()").asString();
-				System.out.println("path RServe;" + path);
-				
-				conn.close();
-				conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
-				
-				
-				
-				
-				  //REXPGenericVector ris = (REXPGenericVector)conn.eval("source('script//regedit//prova.R')");
-				  //System.out.println(ris.asList().at("value").asString());
-				 
-					/*
-					 * conn.close();
-					 * 
-					 * System.out.println(ris.asList().at("value").asString()); conn =
-					 * open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
-					 * conn.eval("setwd('//home2//ruser')").toString();
-					 * 
-					 * String path2 = conn.eval("getwd()").asString();
-					 * System.out.println("path RServe;" + path2);
-					 */
-				//REXPGenericVector ris2 = (REXPGenericVector) conn.eval("source('script//regedit//connessionetest.R')");
-				//REXP ris2 = conn.eval("source('script//regedit//connessionetest.R')");
-				  
-				  
-				  
-				  
-				conn.eval("setwd('//home2//ruser')").toString();
-				path = conn.eval("getwd()").asString();
-				System.out.println("path RServe;" + path);
-				//REXPGenericVector ris2 = (REXPGenericVector) conn.eval("source('R_directory_test//LinkageProbabilistico.R')");
-				//System.out.println(ris2.asList().at("value"));
-				REXP rResponseObject =  conn.parseAndEval("try(source('R_directory_test//LinkageProbabilistico.R'), silent=TRUE)");
-				
-				REXP ris2 = conn.eval("outstr");
-				System.out.println(ris2.asString());
-				//System.out.println(rResponseObject.asString());
-				/*
-				 * REXP ris1 = conn.eval("oraodbcname<-'PD_RSI_PT'" );
-				 * System.out.print("Assegnazione DB:" + ris1.asStrings()[0]);
-				 */
-				
-				/*
-				 * REXP ris2 = conn.eval("orauser<-'UP_STEFANO_MACONE'");
-				 * System.out.print("Assegnazione utente DB:" + ris2.asStrings()[0]);
-				 * 
-				 * REXP ris3 = conn.eval("orapassword<-'Malcomx#1971'");
-				 * System.out.print("Assegnazione password DB:" + ris3.asStrings()[0]);
-				 */
-				
-				/*
-				 * REXP ris4 = conn.eval("PROV<-'Sassari'");
-				 * System.out.print("Assegnazione Provincia:" + ris4.asStrings()[0]);
-				 * 
-				 * REXP ris5 = conn.eval("COD<-'090'");
-				 * System.out.print("Assegnazione Codice Provincia:" + ris5.asStrings()[0]);
-				 * 
-				 * REXP ris6 = conn.eval("s<- 0.6"); System.out.print("Assegnazione Soglia" +
-				 * ris6.asStrings()[0]);
-				 * 
-				 * 
-				 * 
-				 * 
-				 * 
-				 * 
-				 * REXP ris7 = (REXPGenericVector)
-				 * conn.parseAndEval("source('script//regedit//indirizzi_rodbc_regedit_mod.R')")
-				 * ;
-				 * 
-				 * System.out.print("Output Esecuzione Script:" +
-				 * ris7.asList().at("value").asString());
-				 * 
-				 * REXP ris8 = (REXPGenericVector) conn.eval("result')");
-				 * 
-				 * System.out.print("Output Script:" + ris8.asList().at("value").asString());
-				 * conn.close();
-				 */
-				
-				conn.close();
-				
-			
-		} catch (RserveException e) {
-			// TODO Auto-generated catch block
-			conn.close();
-			e.printStackTrace();
-		}
-	}	
-	
+	/*
+	 * @Override public void run(String... args) throws Exception { // TODO
+	 * Auto-generated method stub
+	 * System.out.println("Connessione Server R Starting");
+	 * 
+	 * RConnection conn=null; try {
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
+	 * System.out.println("Connessione Server R:" + conn.toString()); risultato +=
+	 * conn.eval("3 * 5").asString();
+	 * System.out.println("Risultato moltiplicazione R;" + risultato); conn.close();
+	 * //conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023"); conn =
+	 * open(null,Rserver,Rport,Ruser,Rpass);
+	 * System.out.println("Connessione Server R:" + conn.toString());
+	 * conn.eval("setwd('//home2//ruser')").toString();
+	 * //conn.eval("setwd(Rwd)").toString(); String path =
+	 * conn.eval("getwd()").asString(); System.out.println("path RServe;" + path);
+	 * 
+	 * conn.close(); conn = open(null,"rstudio8.istat.it",6311,"ruser","Ruser2023");
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * conn.eval("setwd('//home2//ruser')").toString(); path =
+	 * conn.eval("getwd()").asString(); System.out.println("path RServe;" + path);
+	 * //REXPGenericVector ris2 = (REXPGenericVector)
+	 * conn.eval("source('R_directory_test//LinkageProbabilistico.R')");
+	 * //System.out.println(ris2.asList().at("value")); REXP rResponseObject = conn.
+	 * parseAndEval("try(source('R_directory_test//LinkageProbabilistico.R'), silent=TRUE)"
+	 * );
+	 * 
+	 * REXP ris2 = conn.eval("outstr"); System.out.println(ris2.asString());
+	 * 
+	 * 
+	 * conn.close();
+	 * 
+	 * 
+	 * } catch (RserveException e) { // TODO Auto-generated catch block
+	 * conn.close(); e.printStackTrace(); } }
+	 */
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
---
+++ regedit-backend-service/src/main/java/it/istat/mec/regedit/domain/Archivio.java
@@ -0,0 +1,52 @@
+package it.istat.mec.regedit.domain;
+
+/**
+ * Copyright 2020 ISTAT
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence. You may
+ * obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * Licence for the specific language governing permissions and limitations under
+ * the Licence.
+ *
+ * @author Francesco Amato <framato @ istat.it>
+ * @author Mauro Bruno <mbruno @ istat.it>
+ * @author Renzo Iannacone <iannacone @ istat.it>
+ * @author Stefano Macone <macone @ istat.it>
+ * @version 1.0
+ */
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "WP1_TP_ARCHIVIO")
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Archivio implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name = "CODICE_ARCHIVIO")
+	private Integer codiceArchivio;
+	@Column(name = "DESCRIZIONE_ARCHIVIO")
+	private String descrizioneArchivio;	
+}
